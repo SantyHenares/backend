87e5145e6444e7782ce8ce74f1f93535ccfc3df1
Santiago Henares <santiagodhenares@gmail.com>
2023-01-18 19:02:12 -0300

    Correccion de errores

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,36 @@
 const socket = io();
 
-const ContainerProductos = document.getElementById('product-container');
-const ContainerRealTime = document.getElementById('realtime-container');
+const ContainerProductos = document.getElementById("product-container");
+const ContainerRealTime = document.getElementById("realtime-container");
 
 socket.on("productsRealTime", () => {
   listadoDeProductos(ContainerRealTime);
 });
 
-const listadoDeProductos = async (container) => {
-  const resp = await fetch('../products.json')
-  const data = await resp.json()
+const getFile = async (path) => {
+  const response = await fetch(path).then((res) => {
+    return res.json();
+  });
+  return response;
+};
 
-  data.forEach(elem => {
-      const div = document.createElement('div');
-      div.classList.add('col-sm-3');
-      div.innerHTML = `<div class="card mb-4" style="width: 18rem;">
+const listadoDeProductos = async (container) => {
+  const data = await getFile("/products.json");
+
+  data.forEach((elem) => {
+    const div = document.createElement("div");
+    div.classList.add("col-sm-3");
+    div.innerHTML = `<div class="card mb-4" style="width: 18rem;">
       <img src="${elem.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${elem.title}</h5>
         <p class="card-text">$${elem.price}</p>
         <a href="#" class="btn btn-primary">Ir a comprar</a>
       </div>
-    </div>`
+    </div>`;
     container.appendChild(div);
   });
-}
+};
 
 listadoDeProductos(ContainerProductos);
+listadoDeProductos(ContainerRealTime);
